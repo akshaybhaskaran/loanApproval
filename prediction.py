d36a2882469c84a6c4269fb912d084ec4bac902a
Akshay Bhaskaran <bhaskaran.a@husky.neu.edu>
2019-02-15 00:15:34 -0600

    started code

---
+++ prediction.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+train = pd.read_csv('train.csv', delimiter=",")
+test = pd.read_csv('test.csv', delimiter=",")
+
+'''
+SHAPE OF DATA SET
+train = 614 x 13
+test = 367 x 12
+'''
+
