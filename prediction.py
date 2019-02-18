7dedd44b486d8ddca756073abc720a948ea7e5c5
Akshay Bhaskaran <bhaskaran.a@husky.neu.edu>
2019-02-17 22:21:14 -0600

    prepping data

--- prediction.py
+++ prediction.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.preprocessing import Imputer
+from sklearn import preprocessing
 
 train = pd.read_csv('train.csv', delimiter=",")
 test = pd.read_csv('test.csv', delimiter=",")
@@ -9,4 +10,31 @@ SHAPE OF DATA SET
 train = 614 x 13
 test = 367 x 12
 '''
+
+# Filling missing values with 'Mean'
+train['Credit_History'].fillna(train['Credit_History'].mean(), inplace=True)
+train['Loan_Amount_Term'].fillna(train['Loan_Amount_Term'].mean(), inplace=True)
+train['LoanAmount'].fillna(train['LoanAmount'].mean(), inplace=True)
+
+# Filling missing values with 'Forward Fill'
+train['Gender'].fillna(method='ffill', inplace=True)
+train['Married'].fillna(method='ffill', inplace=True)
+train['Self_Employed'].fillna(method='ffill', inplace=True)
+train['Dependents'].fillna(method='ffill', inplace=True)
+
+# Dropping Loan_ID feature as it doesn't contribute much
+train.drop('Loan_ID', axis=1, inplace=True)
+
+print(train.columns)
+
+
+
+for i in train.columns:
+    if train[i].dtype == 'O':
+        print(set(train[i].tolist()))
+
+
+
+
+
 
